20a4e09a87ae1eee79246fadb2fb5219c04b0b3d
ThiagoCorreiaDev <tcorreia.dev@gmail.com>
2024-07-02 17:03:52 -0300

    Adicionado console.log para retorno de mensagem informando level do jogador.

--- index.js
+++ index.js
@@ -8,22 +8,23 @@ function getHeroLevel(heroXp){
     else if (heroXp >= 1001 && heroXp < 2000){
         return "Bronze"
     }
-    else if (heroXp >= 2001 && heroXp < 3000){
+    else if (heroXp >= 2001 && heroXp < 5000){
         return "Prata"
     }
-    else if (heroXp >= 3001 && heroXp < 4000){
+    else if (heroXp >= 5001 && heroXp < 6000){
         return "Ouro"
     }
-    else if (heroXp >= 4001 && heroXp < 5000){
+    else if (heroXp >= 6001 && heroXp < 7000){
         return "Platina"
     }
-    else if (heroXp >= 5001 && heroXp < 6000){
+    else if (heroXp >= 7001 && heroXp < 8000){
         return "Ascendente"
     }
-    else if (heroXp >=6001 && heroXp < 7000){
+    else if (heroXp >= 8001 && heroXp < 10000){
         return "Imortal"
     }
-    else (heroXp >= 7001);{
+    else if (heroXp >= 10001){
         return "Radiante"
     }
+console.log ("O Herói de nome " + heroName + " está no nível " + getHeroLevel(heroXp));
 }
